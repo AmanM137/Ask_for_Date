58fabc1dc4d285f624d333fb3fc2e2f8d6b38277
Aman Kumar Singh <aman20012004@gmail.com>
2024-01-15 01:03:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
-import "./public/style.css";
+import "./Components/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
